e64ec949b2b9ac7c549e5c9634fbdacc201dd2e1
killinq-joke <zakadmaster@gmail.com>
2020-04-23 18:29:14 +0200

    Addition of pg

--- knexfile.js
+++ knexfile.js
@@ -25,4 +25,15 @@ module.exports = {
       directory: "./data/seeds",
     },
   },
+  production: {
+    client: "sqlite3",
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };
